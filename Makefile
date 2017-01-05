be053176b3c76a81d27bd143f6158782c43e1722
MarxBro <allthemarxbroters@gmail.com>
2017-01-04 23:23:21 -0300

    sonidos

--- Makefile
+++ Makefile
@@ -6,3 +6,5 @@ clean:
 install:
 	cp tomatito.sh /usr/bin/tomatito
 	chmod +x /usr/bin/tomatito
+	cp alert.mp3 /usr/share/sounds/alert.mp3
+	chmod +r /usr/share/sounds/alert.mp3
--- tomatito.sh
+++ tomatito.sh
@@ -21,13 +21,21 @@ ayuda () {
 
 [ "$1" ] && ayuda && exit 0
 
+SILENCIO=1                      # Opcional. Cero -0- para alertas sonoras.
 POMODORO=1500                   # 25 minutos
 POMODORO_RECREO=600             # 5 minutos
 POMODORO_CONTADOR=0             # Al cuarto, recreo largo...
 POMODORO_RECREO_LARGO=1200      # Largo de veinte minutos.
 
 # Funciones
+campana (){
+    if (( SILENCIO == 0)); then
+        mplayer /usr/share/sounds/alert.mp3 2&1>/dev/null
+    fi
+}
+
 notificar (){
+    campana
     notify-send "$1"
 }
 
@@ -35,12 +43,12 @@ notificar_comienzo (){
     notificar 'Comenzando a trabajar: pomodoro!'
 }
 
-notificar_final (){
+notificar_final_pomodoro (){
     notificar 'Pomodoro finalizado. Ahora es tu recreo de 5 minutos.'
     sleep $POMODORO_RECREO && pomodoro_start
 }
 
-notificar_FIN (){
+notificar_FIN_CICLO (){
     notificar 'Pomodoro finalizado. Este es el cuarto, así que ahora es tu recreo de 20 minutos.'
     sleep $POMODORO_RECREO_LARGO && notificar_pomodoro_reinicio
 }
@@ -56,9 +64,9 @@ pomodoro_start(){
     sleep $POMODORO
     if ((POMODORO_CONTADOR == 4)); 
         then
-            notificar_FIN
+            notificar_FIN_CICLO
         else
-            notificar_final
+            notificar_final_pomodoro
     fi
 }
 
